50f32a1bae8d3ec93669c5175af806d7d7ac9735
keithc17 <KeithNorris@Keiths-MacBook-Pro.local>
2014-08-31 22:54:49 -0600

    step1

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -17,7 +17,39 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
+    // Declare two intergers
+    CGFloat karl;
+    CGFloat leonard;
+    
+    //set value of Karl to 10 and leonard to 7
+    karl = 10;
+    leonard = 7;
+    
+    // Declare two more intergers
+    CGFloat karlPlace;
+    CGFloat leonardPlace;
+    
+    //if karl >7 set karlPlace to 1 else 2
+    if (karl < 7) {
+        karlPlace = 1;
+    }
+    else
+    {
+        karlPlace = 2;
+    }
+    
+    //leonardPlace set to 1 or 2
+    leonardPlace = leonard > 7 ? 1 : 2;
+    
+    //Check if leonardPlace and karlPlace are both equal to 1
+    
+    
+    
+    
+    //Log that into the consol
+    NSLog(@"%f",bodyMassIndex);
+    
+    
     
     
     return YES;
